40705022964f861241dd7629209fd2ab24b16e3e
FelRF <89205473+FelRF@users.noreply.github.com>
2023-07-11 14:19:41 -0300

    Update sistema_bancario.py

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -24,32 +24,38 @@ EXTRATO:
 listagem, deve ser exibido o saldo atual da conta.
 """
 
+from datetime import datetime
+
 colorir = {
     "Vermelho":'\033[31m',
     'Verde':'\033[32m',
     'Fecha_Cor':'\033[m'
 }
 
-
 class Operacoes:
-    def sacar(self, valor):
-
+    def sacar(self, valor, hora):
         match valor:
             case _ as quantidade if quantidade > 0 and quantidade <= 500:
                 self.saldo -= valor
-                self.dados_extrato["Quantidade de Saques"]+=1
+                self.dados_extrato["Quantidade de Saques"] += 1
+                self.dados_extrato["Informações dos Saques"]["Valores"].append(valor)
+                self.dados_extrato["Informações dos Saques"]["Horários"].append(hora)
                 print(self.saldo)
                 print(self.dados_extrato["Quantidade de Saques"])
-
-
-    def depositar(self, valor):
+                print(self.dados_extrato["Informações dos Saques"]["Valores"])
+                print(self.dados_extrato["Informações dos Saques"]["Horários"])
+                hora = ''
+    def depositar(self, valor, hora):
         match valor:
-            case _ as quantidade if quantidade >=1:
+            case _ as quantidade if quantidade >= 1:
                 self.saldo += valor
-                self.dados_extrato["Quantidade de Depósitos"] +=1
+                self.dados_extrato["Quantidade de Depósitos"] += 1
+                self.dados_extrato["Informações dos Depósitos"]["Valores"].append(valor)
+                self.dados_extrato["Informações dos Depósitos"]["Horários"].append(hora)
                 print(self.saldo)
-                print(self.dados_extrato["Quantidade de Depósitos"])          
-
+                print(self.dados_extrato["Quantidade de Depósitos"])
+                print(self.dados_extrato["Informações dos Depósitos"]["Valores"])
+                print(self.dados_extrato["Informações dos Depósitos"]["Horários"])
 
 
 class Sistema_Bancario(Operacoes):
@@ -57,10 +63,10 @@ class Sistema_Bancario(Operacoes):
         self.menu = menu
         self.saldo = 0
         self.dados_extrato = {
-            "Quantidade de Saques":0,
-            "Valores dos Saques":[],
-            "Quantidade de Depósitos":0,
-            "Valores dos Depósitos":[],
+            "Quantidade de Saques": 0,
+            "Informações dos Saques": {"Valores":[], "Horários":[]},
+            "Quantidade de Depósitos": 0,
+            "Informações dos Depósitos": {"Valores": [], "Horários":[]},
         }
 
 
@@ -75,7 +81,7 @@ class Sistema_Bancario(Operacoes):
         while True:
             try:
                 escolha_do_usuario = input('\nEscolha o procedimento a ser feito digitando\n'
-                                           'o número correspondente a opção:  ').strip()    
+                                           'o número correspondente a opção:  ').strip()
 
                 digitou_letra = any([caracter.isalpha() for caracter in escolha_do_usuario])
 
@@ -85,13 +91,16 @@ class Sistema_Bancario(Operacoes):
                     case _:
                         match escolha_do_usuario:
                             case "":
-                                raise ValueError(f'\n{colorir["Vermelho"]}ERRO! Você precisa digitar alguma opção, tente novamente.{colorir["Fecha_Cor"]}')
+                                raise ValueError(f'\n{colorir["Vermelho"]}ERRO! Você precisa digitar alguma opção, '
+                                                 f'tente novamente.{colorir["Fecha_Cor"]}')
                             case _ as status if  status.isalnum() == False:
-                                raise ValueError(f'\n{colorir["Vermelho"]}ERRO! Digite somente números!{colorir["Fecha_Cor"]}')
+                                raise ValueError(f'\n{colorir["Vermelho"]}ERRO! Digite somente números!'
+                                                 f'{colorir["Fecha_Cor"]}')
                             case _:
                                 escolha_do_usuario = int(escolha_do_usuario)
                                 if escolha_do_usuario > 4 or escolha_do_usuario < 1:
-                                    raise ValueError(f'\n\n{colorir["Vermelho"]}ERRO! Você digitou uma opção inválida, tente novamente.{colorir["Fecha_Cor"]}')
+                                    raise ValueError(f'\n\n{colorir["Vermelho"]}ERRO! Você digitou uma opção inválida, '
+                                                     f'tente novamente.{colorir["Fecha_Cor"]}')
 
             except ValueError as erro:
                 print(erro)
@@ -111,22 +120,27 @@ class Sistema_Bancario(Operacoes):
                     case 4:
                         print(f'\n{colorir["Verde"]}Você escolheu a opção -> {self.menu[3]}{colorir["Fecha_Cor"]}')
                         return self.menu[3]
-    
+
 
 if __name__ == '__main__':
     sistema = Sistema_Bancario(['Depositar', 'Sacar', 'Gerar Extrato', 'Finalizar'])
+
     while True:
+        data = datetime.today()
+        dia = data.date().strftime("%d/%m/%Y")
+        hora = data.time().strftime("%H:%M:%S")
+
         resposta_usuario = sistema.menu_principal()
         match resposta_usuario:
             case "Depositar":
                 valor_deposito = float(input('Informe o valor do deposito  R$: '))
-                sistema.depositar(valor_deposito)
-            
+                sistema.depositar(valor_deposito, hora)
+
             case "Sacar":
                 valor_saque = float(input('Informe o valor do Saque R$: '))
-                sistema.sacar(valor_saque)
-            
+                sistema.sacar(valor_saque, hora)
+
             case "Finalizar":
                 print('Finalizando sistema, obrigado e volte sempre!')
                 break
-            
+
